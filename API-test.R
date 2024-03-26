91b5a0841d4d58b7ac5e1e2a1585c2db30afc767
Susan Vanderplas <srvanderplas@gmail.com>
2024-03-26 14:20:37 -0500

    Updates

--- API-test.R
+++ API-test.R
@@ -1,6 +1,10 @@
 library(httr)
 library(xml2)
 library(rvest)
+library(glue)
+library(tibble)
+library(dplyr)
+library(purrr)
 
 headers = c(
   'accept' = 'application/json',
@@ -11,6 +15,10 @@ headers = c(
 url <- "https://api2.isbndb.com/books/{query}?page={page}&pageSize=1000&column={columnName}"
 
 
-url <- "https://api2.isbndb.com/books/science%20fiction?page=1&pageSize=1000&column=subjects"
+url <- glue("https://api2.isbndb.com/books/science%20fiction?page={i}&pageSize=1000&column=subjects", i = 1:10)
 
-resp <- GET(url, add_headers(headers))
+resp <- tibble(url = url, resp = purrr::map(url, ~GET(., add_headers(headers))))
+
+resp <- resp %>% mutate(df = map(resp, ~jsonlite::parse_json(content(., as = "text"), simplifyVector=T)))
+
+df = jsonlite::parse_json(content(resp, as = "text"), simplifyVector = T)
